84367b1c49e79d7463a3e3d0006c0fb013057c18
KasukebeYou <135225215+KasukebeYou@users.noreply.github.com>
2023-06-30 01:53:53 +0800

    Update patch_perseus.sh

--- patch_perseus.sh
+++ patch_perseus.sh
@@ -25,17 +25,10 @@ done
 chmod +x apkeep
 
 # Download Azur Lane
-download_azurlane () {
     if [ ! -f "com.YoStarJP.AzurLane" ]; then
-    ./apkeep -a com.YoStarJP.AzurLane .
-    fi
-}
-
-if [ ! -f "com.YoStarJP.AzurLane" ]; then
     echo "Get Azur Lane apk"
-    download_azurlane
-    unzip -o com.YoStarJP.AzurLane.xapk -d AzurLane
-    cp AzurLane/com.YoStarJP.AzurLane.apk .
+    wget https://doc-0c-cc-docs.googleusercontent.com/docs/securesc/fdhde8q03ekqattlvclp15m8f2pj9urh/daot8pv57lce66tngtl8no2dhj7lof5p/1688061150000/05309739250916540937/04011455877612054337/19z9SgkJ28XHxpvjglQ6TTLW-K0qvWWBx?e=download&ax=ADWCPKAR7IckohABqFBjWcRgxmOU6QGjlrKLfmysP-psEUb6mZ1Lmt0F5pIkLn3ntn1-vydnhJkxIzKoub-0BBnnnRQofWQgvGb9vKImh_jdDBSVZ3zFqQ7dIEwpk_RBeXZTkltacQFQAYaMOjUWQKjxx0-17IroTZZMc_uaofLbnODpjuQDPWWqQtrPlouR3kBDSS_iiYyNSUGbTrkJ8tY4hIfU-Y3FPbO5iGI3Tdum4SQ_HV-pzioHHoSLYgYpVHJWAlHhNyu7Yl32SKE2ZuupLTvSiK-cVjCYV4Jw-tnYjsnr46bgx1O2QEMhRSoWrJj50kZqHV1sWkjYEmEx89wg9o7UvbD53DeqsJ6bo2uZpWGDN6nywyRkLqsVQVTK1s2B4Y_wr6XTYfIj8_21vfz2WdqfIi5gztyddk-nPh3jGapspbS9rA2BxEas8uqo9BoTORKwQ3YxqDUjfDSLKuDpUTOZMqAnC_TdwlD5HXXn0TrXB1baUU6gSs46ogiQXbImwfmuyEKCBHZSKd1lmRgRknxtuFZaZ-Tou5V6OyvwSicWR5qHraJVd0YJDSe9ksFfgyobGX3KbyvIbNUjXxdyg56zXAHcAuLaqCrjxmylV47wKhtYnIsWq4cozXNK_gXd2rcYibJ4n1k2swdPx_abwyaRSaOb4FiifWYMNZv464G17KUwrdFlNtQU0iEfsbuP3RdnDIVIDDRGXX0oqytT-SGGHCpocpjPfxaVoIvRBoN25wwkMS5fsphXXky23CsFeZUmZUM8KQlIdLTHYbPHLFB_5lwbWFBpxXYpYuh2b3aAkn7nyP3qaOOJwNOiwGFlGrNezy6r0z4kWZViJFbQxXPAhaC8OCR5UtPMFRltfUhIVjDMkis7-tcc1lBsDzPffQ&uuid=9b610f36-974d-42a6-bb87-61e01bde82a3&authuser=0&nonce=1786bqhhp1ipe&user=04011455877612054337&hash=72vvj74kpp1ab0e95gnodkb0b0gnr9rj -O com.YoStarJP.AzurLane -q
+    echo "apk downloaded !"
 fi
 
 # Download Perseus
