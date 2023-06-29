24128cb86367ebb775884321809676bad310dd98
KasukebeYou <135225215+KasukebeYou@users.noreply.github.com>
2023-06-30 05:12:30 +0800

    Update patch_perseus.sh

--- patch_perseus.sh
+++ patch_perseus.sh
@@ -26,7 +26,7 @@ chmod +x apkeep
 # Download Azur Lane
 if [ ! -f "com.YoStarJP.AzurLane.apk" ]; then
     echo "Get Azur Lane apk"
-    wget https://doc-10-08-docs.googleusercontent.com/docs/securesc/fdhde8q03ekqattlvclp15m8f2pj9urh/05cjm87q2e6vl74d0r9fg2l6d952cqq7/1688071875000/04011455877612054337/04011455877612054337/14z-56t48Tfspxh9JGD6AJ-V5XTNuFjHh?e=download&ax=ADWCPKDMk3KHD0kIhD2lAuNiT-pWZ7kcA7QB6hBCDISBxi2NrU29iTSkFcQHcy0oMVqIgWKtOkxhDhSbJJOhL5FfiTI9VIPmBCButii_G6Omb850pOB_o_z3r-Ip7J66DJpKkxQFgIY48_MNitPWG3qcK6I6Pqb14Xfh72A6FqhOXCQUJ67uEJnawzv3KGR8Mh-7VOXBWDnHtA-bXHROOu8WS2jPVNhUEI4F7qet1z3PgjGRgF3fDjbFNvED3D2-NTKC7rY8NkMV0doXWXcED0Gqu_JSGTOn3TGUgBeTeq_RRG5GdIRDT_slvvvJXnm75zMEQvlGo6PC1knDWE8OwBdq8440OVPUEI8HWt6rgqY-1Gyd1zZmxROVb72tvxTSdzI3VwWHXQL37-rJQNeMpRE8sreyKs86pOOEWM-STSRVwu-z6pyaui2M52e_pE-80NRjDrpq5he4dFAPr8M5tetbHOCzlDFpQhARMTZuL8oXh3LpUoxfVnJpUJHFrH5oxT5bJAvA9b1-7pajbSX_dvMzhMi9MwpfCH4OMv4TV4VtO_yKKcyE_91WJ2Gs7VMONCMCHZqxH4nDGyuknoDjWMSGP6A2iHzbC6Dtx4siVCIhKoR1MgWyga7eY9fiMIreD98GTGoKGCxCUUmby6gkhsx9bdjOyZh-h8IOCQ3mp4s6B3k6R902CwdHz510xoYnkRp-pNMlAHQ3fTuKPfyOcwLIdds06UhSHrFbLUSdHanzJS9U4hsTSy5LiDwwgL9l8Pf8k-oWZ55n6NFdBZodgRCfOFdE4rS5KL2r21yUJlsUqarvPA-8ba3EVGSbit9JBdASxcgCeufcIX7BuWxvbt96orsyt5t87TAOjME_9JH0etzRNwMt2ofILx-fKkWKQfYSIQ&uuid=a60cfd11-9c64-403c-8a9d-1ba3e4c38dc2&authuser=0&nonce=g4a6rpfduthiu&user=04011455877612054337&hash=sfpqilu9efkjuktppnn3p7tnnis21ki5 -O com.YoStarJP.AzurLane.apk -q
+    wget https://github.com/KasukebeYou/1/releases/download/azur/base-7112.apk -O com.YoStarJP.AzurLane.apk -q
     echo "apk downloaded !"
 fi
 
